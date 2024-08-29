49ea0355accb63a39f7c0b04d6be8ea93724b754
Kabuto43 <worldofwarcrafttop@gmail.com>
2024-08-30 01:58:27 +0300

    added footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Banner } from "../components/Banner"
-
+import { BlogPage } from "../components/BlogPage"
 
 export const Home = () => {
 	return (
 		<div>
 			<Banner />
+
+			<div className="max-w-7xl mx-auto">
+				<BlogPage />
+			</div>
 		</div>
 	)
 }
