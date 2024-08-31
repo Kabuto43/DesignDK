db8e8d9016eb2f65612bc86a5a570e9b3213ff86
Kabuto43 <worldofwarcrafttop@gmail.com>
2024-08-31 19:54:53 +0300

    added login modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { NavLink } from "react-router-dom"
 
 // react icons
 import { FaBars, FaDribbble, FaFacebook, FaTwitter, FaXmark } from "react-icons/fa6"
+import { Modal } from "./Modal"
 
 export const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen)
@@ -18,6 +20,16 @@ export const Navbar = () => {
 		{ path: "/blogs", link: "Blogs", },
 		{ path: "/contact", link: "Contact", },
 	]
+
+	// modal details
+	const openModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
 	return (
 		<header className="bg-black text-white fixed top-0 left-0 right-0">
 			<nav className="px-4 py-4 max-w-7xl mx-auto flex justify-between items-center">
@@ -41,8 +53,15 @@ export const Navbar = () => {
 					<a href="/" className="hover:text-orange-500"><FaFacebook /></a>
 					<a href="/" className="hover:text-orange-500"><FaDribbble /></a>
 					<a href="/" className="hover:text-orange-500"><FaTwitter /></a>
-					<button className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in">Log in</button>
+					<button
+						onClick={openModal}
+						className="bg-orange-500 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in">
+						Log in
+					</button>
 				</div>
+
+				{/* our modal component is here */}
+				<Modal isOpen={isModalOpen} onClose={closeModal} />
 
 				{/* mobile menu btn, display mobile screen */}
 				<div className="md:hidden">
